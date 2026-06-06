542fe66ca3a67ed5a65418a8ed284387f5b61890
akumumaria <akumumaria56@gmail.com>
2026-06-06 07:44:57 +0300

    update my flash messages

--- middlewares/flash.js
+++ middlewares/flash.js
@@ -1,13 +1,40 @@
-const flash = require('connect-flash');
-
-/**
- * Flash message middleware
- * Makes flash messages available in all views via res.locals
- */
 const flashMiddleware = (req, res, next) => {
-  res.locals.success_messages = req.flash('success');
-  res.locals.error_messages = req.flash('error');
+  req.flash = function(type, message) {
+    if (!req.session.flash) {
+      req.session.flash = { success: [], error: [], warning: [], info: [] };
+    }
+
+    if (type && message) {
+      if (!req.session.flash[type]) {
+        req.session.flash[type] = [];
+      }
+      req.session.flash[type].push(message);
+      return;
+    }
+
+    if (type && !message) {
+      const messages = req.session.flash[type] || [];
+      req.session.flash[type] = [];
+      return messages;
+    }
+
+    const allMessages = req.session.flash || { success: [], error: [], warning: [], info: [] };
+    req.session.flash = { success: [], error: [], warning: [], info: [] };
+    return allMessages;
+  };
+
   next();
+}
+
+const flashLocals = (req, res, next) => {
+  res.locals.flash = req.flash;
+  next();
+}
+
+const flash = (req, res, next) => {
+  flashMiddleware(req, res, () => {
+    flashLocals(req, res, next);
+  });
 };
 
-module.exports = flashMiddleware;
+module.exports = flash;
